aae14ff934ff340fb657645f107a66b35ed10685
Jacob <jacob.humrich@gmail.com>
2018-10-24 21:55:57 -0700

    Created project. Added WebConfig.java.

---
+++ src/main/java/net/sytes/jacobhumrich/JacobhumrichApplication.java
@@ -0,0 +1,12 @@
+package net.sytes.jacobhumrich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacobhumrichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacobhumrichApplication.class, args);
+	}
+}
