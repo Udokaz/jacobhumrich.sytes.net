434aec324af4d087aa1ca83eeed4ce3d4ccbb107
Jacob <jacob.humrich@gmail.com>
2018-11-02 09:59:58 -0700

    Added in controller classes.

---
+++ src/main/java/net/sytes/jacobhumrich/CalculatorController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.sytes.jacobhumrich;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author jacob
+ */
+@Controller
+@RequestMapping("/Calculator")
+public class CalculatorController {
+    
+    
+}
