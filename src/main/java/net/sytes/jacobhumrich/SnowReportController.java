7e4d0c8ec3b3ef2c7294f8fd5c5f30e64b21a888
jacob humrich <jacob.humrich@gmail.com>
2019-01-21 15:48:35 -0800

    weather api is out of date. Removed the code for it. Started updating snowreportcontroller to be a resetful service.

--- src/main/java/net/sytes/jacobhumrich/SnowReportController.java
+++ src/main/java/net/sytes/jacobhumrich/SnowReportController.java
@@ -6,20 +6,25 @@
 package net.sytes.jacobhumrich;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author jacob
  */
-@Controller
-@RequestMapping("/snowreport")
+@RestController
+@RequestMapping(path = "/snowreport",
+                produces="appilication/json")
+@CrossOrigin(origins="*")
 public class SnowReportController {
     
     
-    @GetMapping
-    public String home(){
+    @GetMapping("/{location}")
+    public String getLocation(@PathVariable("location") long id){
         return "snowreport";
     }
 }
